0ee7bc0fbf385cb081af019fa0f33e6ad6b1c3b5
Jason Wang <954014787@qq.com>
2023-08-19 18:55:56 +0800

    set default temperature 0.7

--- start.bat
+++ start.bat
@@ -6,4 +6,5 @@ set CHATBOT_HOME_PAGE=https://www.google.com
 set CHATBOT_BRAND_NAME=AIYAXTEST
 set NEXT_PUBLIC_MODELS_URL=http://localhost:8080/openai/v1/models
 set NEXT_PUBLIC_COMPLETIONS_URL=http://localhost:8080/openai/v1/chat/completions
+@REM set NEXT_PUBLIC_DEFAULT_TEMPERATURE=0.7
 npm run dev
