14d46ae0df02b4f7c51479ebed458203302fc21f
Jason Wang <954014787@qq.com>
2024-01-04 22:52:35 +0800

    Add docker build script

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -5,11 +5,5 @@ REGISTRY="ghcr.io"
 IMAGE_NAME="aiyaxcom/chatbot-ui"
 TAG="main"
 
-# Set up QEMU
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-
-# Setup Docker buildx
-docker buildx create --use
-
 # Build Docker image
-docker buildx build --platform linux/amd64,linux/arm64 -t $REGISTRY/$IMAGE_NAME:$TAG .
+docker build -t $REGISTRY/$IMAGE_NAME:$TAG .
