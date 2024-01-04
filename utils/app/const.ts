ccc6d598a3db84da3d8457ae687dc30e85ccca87
Jason Wang <954014787@qq.com>
2024-01-05 00:07:09 +0800

    Remove useless dependency

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -2,23 +2,8 @@ export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
   "You are an AI chatbot, providing chat service. Follow the user's instructions carefully. Respond using markdown.";
 
-export const OPENAI_API_HOST =
-  process.env.OPENAI_API_HOST || 'https://api.openai.com';
-
-export const DEFAULT_TEMPERATURE = 
+export const DEFAULT_TEMPERATURE =
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "0.7");
-
-export const OPENAI_API_TYPE =
-  process.env.OPENAI_API_TYPE || 'openai';
-
-export const OPENAI_API_VERSION =
-  process.env.OPENAI_API_VERSION || '2023-03-15-preview';
-
-export const OPENAI_ORGANIZATION =
-  process.env.OPENAI_ORGANIZATION || '';
-
-export const AZURE_DEPLOYMENT_ID =
-  process.env.AZURE_DEPLOYMENT_ID || '';
 
 export const CHATBOT_BRAND_NAME =
     process.env.CHATBOT_BRAND_NAME || 'AIYAX';
