142ee5368f538978d525917f894378a73e492fed
Jason Wang <954014787@qq.com>
2024-01-14 23:58:59 +0800

    重构会员中心对话框

--- components/Settings/VipDialog.tsx
+++ components/Settings/VipDialog.tsx
@@ -1,4 +1,5 @@
 import {FC, useContext, useEffect} from 'react';
+import { Table } from 'antd';
 import { useTranslation } from 'next-i18next';
 import Cookies from 'js-cookie';
 import { IconVip, IconVipOff, IconX, IconMoodBoy } from '@tabler/icons-react';
@@ -29,6 +30,59 @@ export const VipDialog: FC<Props> = ({ open, onClose }) => {
     const expiryDate = expiryTimestamp ? new Date(Number(expiryTimestamp)).toLocaleString() : '无';
     const serverUrl = Cookies.get('serverUrl');
 
+    const products = [
+        {membership: '黄金会员', period: '1天', description: '描述', price: '9元', link: `${serverUrl}/purchase/product/1`},
+        {membership: '黄金会员', period: '1周', description: '描述', price: '39元', link: `${serverUrl}/purchase/product/2`},
+        {membership: '黄金会员', period: '1月', description: '描述', price: '99元', link: `${serverUrl}/purchase/product/3`},
+    ];
+
+    const benefits = [
+        {type: '基础版', nonMember: '30次/日', member: '80次/日'},
+        {type: '高级版', nonMember: '不可使用', member: '20次/日'},
+    ];
+
+    const className = 'bg-white dark:bg-[#202123] text-black dark:text-neutral-400';
+    const rowClassName = 'hover:bg-gray-500';
+
+    const productColumns = [
+        {
+            title: '会员类型',
+            dataIndex: 'membership',
+            key: 'membership',
+            className: className
+        },
+        {
+            title: '购买时长',
+            dataIndex: 'period',
+            key: 'period',
+            className: className
+        },
+        {
+            title: '价格',
+            dataIndex: 'price',
+            key: 'price',
+            className: className
+        },
+        {
+            title: "购买链接",
+            dataIndex: 'link',
+            key: 'link',
+            render: (text: string) => <a href={text} target="_blank" className="underline hover:underline">购买</a>,
+            className: className
+        },
+    ];
+
+    const benefitColumns = [
+        {
+            title: '聊天模型',
+            dataIndex: 'type',
+            key: 'type',
+            className: className,
+        },
+        { title: '非会员', dataIndex: 'nonMember', key: 'nonMember', className: className },
+        { title: '黄金会员', dataIndex: 'member', key: 'member', className: className },
+    ];
+
     if (!open) {
         return <></>;
     }
@@ -39,7 +93,7 @@ export const VipDialog: FC<Props> = ({ open, onClose }) => {
                 <div className="flex items-center justify-center min-h-screen px-4 text-center sm:block sm:p-0">
                     <div className="hidden sm:inline-block sm:h-screen sm:align-middle" aria-hidden="true" />
 
-                    <div className="inline-block max-h-[650px] transform rounded-lg border border-gray-300 bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all dark:bg-[#202123] sm:my-8 sm:max-h-[650px] sm:w-full sm:max-w-lg sm:p-6 sm:align-middle">
+                    <div className="inline-block max-h-[650px] transform rounded-lg border border-gray-300 bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all dark:bg-[#202123] sm:my-8 sm:max-h-[700px] sm:w-full sm:max-w-lg sm:p-6 sm:align-middle">
                         <button type="button" className="absolute top-1 right-1 text-black dark:text-neutral-200" onClick={onClose}>
                             <IconX size={18} />
                         </button>
@@ -49,32 +103,39 @@ export const VipDialog: FC<Props> = ({ open, onClose }) => {
                                 <img src={avatar} alt="User Avatar" className="rounded-full" width="50" height="50" />
                             ) : (<IconMoodBoy size={50} />)}
                             <div className="text-xl font-bold text-black dark:text-neutral-200">{nickname}</div>
-                        </div>
 
-                        <div className="mb-6">
-                            <p className={member ? "text-green-500" : "text-yellow-500"}>{member ? "可享每日20次高级版（GPT-4-Turbo）对话" : "开通会员可享每日20次高级版（GPT-4-Turbo）对话" }</p>
                         </div>
 
                         {member ? (
-                            <div className="flex items-center space-x-2 mb-6 text-black dark:text-neutral-200">
+                            <div className="flex items-center space-x-2 mb-6 text-green-500">
                                 <IconVip size={18} color="gold"/>
-                                <p>会员到期时间: {expiryDate}</p>
+                                <p>黄金会员 到期时间: {expiryDate}</p>
                             </div>
                         ) : (
-                            <div className="flex items-center space-x-2 mb-6 text-black dark:text-neutral-200">
+                            <div className="flex items-center space-x-2 mb-6 text-yellow-500">
                                 <IconVipOff size={18}/>
                                 <p>非会员</p>
                             </div>
                         )}
 
-                        <div className="mb-6">
-                            <p className="mb-4 text-black dark:text-neutral-200">{member ? "续费会员：" : "开通会员："}</p>
-                            <div className="flex flex-col space-y-2 text-black dark:text-neutral-200">
-                                <div className="flex justify-between items-center space-x-2 border p-2 rounded hover:bg-green-500 transition-colors">日卡 <span>9元</span> <a href={`${serverUrl}/purchase/product/1`} className="btn" target="_blank">购买</a></div>
-                                <div className="flex justify-between items-center space-x-2 border p-2 rounded hover:bg-green-500 transition-colors">周卡 <span>39元</span> <a href={`${serverUrl}/purchase/product/2`} className="btn" target="_blank">购买</a></div>
-                                <div className="flex justify-between items-center space-x-2 border p-2 rounded hover:bg-green-500 transition-colors">月卡 <span>99元</span> <a href={`${serverUrl}/purchase/product/3`} className="btn" target="_blank">购买</a></div>
-                            </div>
+                        <div className="mb-4">
+                            <div className="font-bold text-black dark:text-neutral-200">购买会员</div>
+                            <Table
+                                   rowClassName={rowClassName}
+                                   columns={productColumns}
+                                   dataSource={products}
+                                   showHeader={false}
+                                   pagination={false} />
                         </div>
+
+                        <div className="my-6"></div>
+
+                        <div className='mb-4'>
+                            <div className="font-bold text-black dark:text-neutral-200">会员权益说明</div>
+                            <Table columns={benefitColumns} dataSource={benefits} pagination={false} />
+                        </div>
+
+                        <div className="my-6"></div>
 
                         <a href={`${serverUrl}/account/`} target="_blank" rel="noopener noreferrer" className="hover:underline text-black dark:text-neutral-200">进入我的账户</a>
                     </div>
